04e419817a0a4cf89e0e5778466f33019f2a5910
Amy <amyschlesener@gmail.com>
2013-02-28 15:43:25 -0800

    added loop to generate multiple OTPs

--- OTP/generateOTP.java
+++ OTP/generateOTP.java
@@ -9,8 +9,6 @@ import java.util.Arrays;
  //SHA-2 hash function implementation adapted from http://www.mkyong.com/java/java-sha-hashing-example/
 
 
-
-
 public class generateOTP
 {
     public static void main(String[] args)throws Exception
@@ -24,14 +22,30 @@ public class generateOTP
  	//SecureRandom randomGenerator = new SecureRandom();
 	//byte[] randomNumber = new byte[20];
 	//randomGenerator.nextBytes(randomNumber);
-
+ 	String storeHash;
+	String storePassword;
+	//do first run with intialization vector
 	generateOTP firstRun = new generateOTP();
- 	String hashTest = firstRun.genHash(IV);
-	firstRun.genPassword(hashTest); 
-}
-	public String genHash(String IV)throws Exception {
+ 	storeHash = firstRun.genHash(IV);
+	storePassword = firstRun.genPassword(storeHash); 	
+
+	//firstRun.genPassword(hashTest); 
+//function to study collision property: generate 100,000 OTP, determine collision property
+	for(int i = 0; i < 2; i++) {
+	rand = new Random();
+	randomNum = rand.nextInt();
+	IV = Integer.toString(randomNum);
+	System.out.println("Random number (IV): "+ IV);
+	firstRun = new generateOTP();
+ 	storeHash = firstRun.genHash(IV);
+	storePassword = firstRun.genPassword(storeHash); 
+	}
+
+
+}	//this is where we generate our hash 
+	public String genHash(String input)throws Exception {
        		MessageDigest md = MessageDigest.getInstance("SHA-256");
-       		 md.update(IV.getBytes());
+       		 md.update(input.getBytes());
        		 byte byteData[] = md.digest();
         	//convert the byte to hex format method 1
         	StringBuffer sb = new StringBuffer();
@@ -44,7 +58,7 @@ public class generateOTP
 	}
 	
 	public String genPassword(String hash) {
-	//now we need to generate numbers based on the SHA hash output. so take last 12 chars of sb string, 		convert in pairs to integer values
+	//now we need to generate numbers based on the SHA hash output. so take last 12 chars of sb string, 		convert in pairs to integer values (usually ends up being only first few hex values used to make 6-int long number string) 
 		char[] hashArray = hash.toCharArray();
 		char[] shortHashArray;
 		shortHashArray = new char[12];
@@ -52,7 +66,7 @@ public class generateOTP
 		for(int i = 0; i < 12; i++) {
 			shortHashArray[i] = hashArray[52+i]; //shortHashArray is last 12 digits of entire hash
 		}
-
+		
 		String Password = "";
 		int hexNum;
 		for (int i = 0; i < 6; i++) {
@@ -78,9 +92,6 @@ public class generateOTP
 		System.out.println("OTP: " + Password);
 		return Password;
 	}
-
-
-
 
 }
 
